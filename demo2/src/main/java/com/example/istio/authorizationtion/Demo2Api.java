80186df3c55d40b2e265a767ddedfd3dd3113fca
rodrigues.vieira.flavio@gmail.com <fkr.company.group@gmail.com>
2020-05-31 20:45:03 -0300

    api

--- demo2/src/main/java/com/example/istio/authorizationtion/Demo2Api.java
+++ demo2/src/main/java/com/example/istio/authorizationtion/Demo2Api.java
@@ -8,9 +8,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class Demo2Api {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/demo2")
     public String greeting() {
         return "hello demo 2";
